3be0bc1df9632d2bc3a120792a3211c4a3cd0d1b
Kanzaki Ning O. Bernardo <kanzakibernardo0@gmail.com>
2026-06-22 17:06:57 +0800

    Updated email credentials
    Removed email credentials and sender information in forgotp_process.php.

--- CrackTheCode/Backend/forgotp_process.php
+++ CrackTheCode/Backend/forgotp_process.php
@@ -11,12 +11,12 @@ function sendOtp($email, $otp) {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'aera.montefalco33@gmail.com';
-    $mail->Password = 'cmcw pnwe fqxx meih'; // App password
+    $mail->Username = ''; // email
+    $mail->Password = ''; // App password
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
 
-    $mail->setFrom('aera.montefalco33@gmail.com', 'CRACK THE CODE');
+    $mail->setFrom('email', 'CRACK THE CODE');
     $mail->addAddress($email);
 
     $mail->isHTML(true);
